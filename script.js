f047117cc92b9200aedf48a780152201069990f1
jake-utk <jakeryanaddis@gmail.com>
2020-10-12 15:02:01 -0500

    javascript still not coming together

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("connection confirmed");
-
 // random notes //
 // make sure to tell the player what to do from the very beginning via the h2.  i mean shit like tell them when the game loads in the browser "Click the same buttons as simon" or something, until a modal is developed to house game instructions.
 
@@ -28,7 +26,6 @@ resetGame.addEventListener("click", (event) => {
 	console.log(event.target);
 	document.getElementById("resetgame").style.visibility = "hidden";
 	document.getElementById("startgame").style.visibility = "visible";
-	// display game reset message
 });
 
 redButton.addEventListener("click", (event) => {
@@ -58,7 +55,6 @@ jamButton.addEventListener("click", (event) => {
 
 startGame.addEventListener("click", (event) => {
 	event.preventDefault();
-	console.log(event.target);
 	document.getElementById("startgame").style.visibility = "hidden";
 	document.getElementById("resetgame").style.visibility = "visible";
 	document.getElementById("messages").innerText = "Simon's turn!";
@@ -70,55 +66,76 @@ startGame.addEventListener("click", (event) => {
 
 function simonPick() {
 	if (currentPlayer === simon) {
-		console.log("Simon picks now");
-		let pick = Math.floor(Math.random() * 4); // generates random value 0-3
+		let pick = Math.floor(Math.random() * 4); 
 		let pickString = buttonArray[pick];
 		console.log(
-			`the value being pushed to array is ${pickString}, the next log is what is currently in the simonChoices array`
+			`the value being pushed to simonChoices array is ${pickString}, the next log is what is currently in the simonChoices array`
 		);
-		simonChoices.push(pickString); // push value into simChoices[] array
+		simonChoices.push(pickString);
 	}
 	return simonSequence();
 }
 
+function playerPick() {
+    if (currentPlayer != player) {
+        currentPlayer = player
+    }
+    addGlow()
+    setTimeout(() => {
+        removeGlow()
+    }, 1000)
+
+}
+
 function simonSequence() {
 	console.log(simonChoices);
-	// loop over the simonChoices array
-	for (let i = 0; i < simonChoices.length; i++) {
-		let pick = simonChoices[i];
-        
-		// setTimeout() a second?
-        // clear the 'glow' class from the buttons
-		setTimeout(() => {
-            removeGlow();
-        }, 1000);
-        
-		let lightSimonPickUp = () => {
-            console.log(pick);
-			console.log(document.getElementById(`${pick}`));
-			document.getElementById(`${pick}`).classList.add("newGlow");
-			console.log(glow);
-			// console.log(document.querySelectorAll(".guitarbuttons"));
-			console.log("player turn now, simons choice light shouldve come on");
-		};
-        
-        // for each value, add 'glow' class
-        // setTimeout() 2 seconds
-		setTimeout(() => {
-            lightSimonPickUp();
-        }, 3000);
-    }
-
-		setTimeout(() => {
-			removeGlow();
-		}, 3000);
-	
+	setTimeout(() => {
+		removeGlow();
+	}, 1000);
+    interval = 2000,
+    simonChoices.forEach((simonChoices, index) => {
+        setTimeout(() => {
+            console.log(simonChoices);
+            let pick = simonChoices
+            document.getElementById(`${pick}`).classList.add("newGlow");
+            removeNewGlow()
+        }, (index+1) * interval)
+    })
+    console.log('when is this produced?');
 }
 
 function removeGlow() {
-	// causes all 4 buttons to dim
 	document.getElementById("red").classList.remove("glow");
 	document.getElementById("blue").classList.remove("glow");
 	document.getElementById("green").classList.remove("glow");
 	document.getElementById("yellow").classList.remove("glow");
 }
+
+function addGlow() {
+    document.getElementById("red").classList.add("glow")
+    document.getElementById("blue").classList.add("glow");
+    document.getElementById("green").classList.add("glow");
+    document.getElementById("yellow").classList.add("glow");
+}
+
+function removeNewGlow() {
+    setTimeout(() => {
+        document.getElementById("red").classList.remove("newGlow");
+        document.getElementById("blue").classList.remove("newGlow");
+        document.getElementById("green").classList.remove("newGlow");
+        document.getElementById("yellow").classList.remove("newGlow");
+    }, 3000)
+}
+
+
+    // FOR LOOP FOR simonSequence()
+	// for (let i = 0; i < simonChoices.length; i++) {
+	// 	let pick = simonChoices[i];
+	// 	(function lightSimonPickUp() {
+	// 		console.log(pick);
+	// 		document.getElementById(`${pick}`).classList.add("newGlow");
+	// 		setTimeout(() => {
+	// 			return lightSimonPickUp();
+	// 		}, 2000);
+	// 	});
+	// }
